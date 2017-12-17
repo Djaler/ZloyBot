98cba41fe5846745c067ae8530de34df6b7ecb96
Kirill Romanov <djaler1@gmail.com>
2017-12-17 15:13:59 +0300

    Ответы на сообщения боту

--- modules/primitive_response.py
+++ modules/primitive_response.py
@@ -1,7 +1,18 @@
 import re
 from random import choice, randint
 
-from telegram.ext import CommandHandler, Filters, MessageHandler
+from telegram.ext import BaseFilter, CommandHandler, Filters, MessageHandler
+
+
+class ReplyToBotFilter(BaseFilter):
+    def filter(self, message):
+        bot_username = message.bot.username
+        reply = message.reply_to_message
+        
+        return bool(reply) and reply.from_user.username == bot_username
+
+
+reply_to_bot_filter = ReplyToBotFilter()
 
 
 class PrimitiveResponse:
@@ -9,10 +20,13 @@ class PrimitiveResponse:
         self._chat_id = chat_id
     
     def add_handlers(self, add_handler):
-        add_handler(MessageHandler(Filters.text, self._run))
+        add_handler(MessageHandler(Filters.text, self.text_responses))
+        add_handler(
+            MessageHandler(Filters.text & reply_to_bot_filter,
+                           self.reply_responses))
         add_handler(CommandHandler('me', self._me, pass_args=True))
 
-    def _run(self, bot, update):
+    def text_responses(self, bot, update):
         def text_response(patterns, answer, chance=100):
             if any(re.search(pattern, text) for pattern in patterns):
                 if answer.endswith('.txt'):
@@ -56,6 +70,24 @@ class PrimitiveResponse:
         text_response([r'\bага$'], 'в жопе нога', 33)
 
         text_response([r'\bнет$'], 'пидора ответ', 10)
+
+    def reply_responses(self, bot, update):
+        def reply_response(patterns, answer, chance=100):
+            if any(re.search(pattern, text) for pattern in patterns):
+                if answer.endswith('.txt'):
+                    answer = self._choice_variant_from_file(answer)
+            
+                if randint(1, 100) <= chance:
+                    bot.sendMessage(chat_id=chat_id, text=answer,
+                                    reply_to_message_id=message_id,
+                                    markdown_support=True)
+    
+        message = update.message
+        chat_id = message.chat_id
+        text = message.text.lower()
+        message_id = message.message_id
+    
+        reply_response(['.*'], "Чё сказал?", 33)
     
     def _me(self, bot, update, args):
         message = update.message
