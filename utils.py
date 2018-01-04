b5e5c522745fc08c15cb0ffc300434053da1b52d
Evgeny Kovalchuk <kovalchuk.evgeny@gmail.com>
2018-01-04 12:23:53 +0200

    Добавлен модуль блокировки стикерпаков

---
+++ utils.py
@@ -0,0 +1,8 @@
+from itertools import zip_longest
+
+import supycache
+
+
+@supycache.supycache(cache_key='admin_ids_{1}', max_age=10 * 60)
+def get_admin_ids(bot, chat_id):
+    return [admin.user.id for admin in bot.get_chat_administrators(chat_id)]
