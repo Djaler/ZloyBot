c7c7b7acd7f8a381b2ea01fd82ea6537c06d4d87
Evgeny Kovalchuk <kovalchuk.evgeny@gmail.com>
2018-01-06 22:02:00 +0200

    Добавлен модуль, который отображает закрепленное сообщение

---
+++ modules/reply_to_pin.py
@@ -0,0 +1,36 @@
+from telegram.ext import CommandHandler
+
+from filters import PermittedChatFilter, supergroup_filter
+
+
+class ReplyToPin:
+    MESSAGES_TO_RESPOND = 50
+
+    def __init__(self, chat_id, admin_id):
+        self._chat_id = chat_id
+        self._admin_id = admin_id
+
+        self.permitted_chat_filter = PermittedChatFilter([self._chat_id])
+
+        self.last_message_id = None
+
+    def add_handlers(self, add_handler):
+        add_handler(CommandHandler('pinned', callback=self.reply_to_pin,
+                                   filters=self.permitted_chat_filter & supergroup_filter))
+
+    def reply_to_pin(self, bot, update):
+        message = update.message
+
+        if self.last_message_id is None:
+            self.last_message_id = message.message_id - self.MESSAGES_TO_RESPOND
+
+        if message.message_id - self.last_message_id < self.MESSAGES_TO_RESPOND:
+            return
+
+        chat = bot.get_chat(message.chat_id)
+        if chat.pinned_message is None:
+            return
+
+        chat.pinned_message.reply_text('☝️️', disable_notification=True)
+
+        self.last_message_id = message.message_id
