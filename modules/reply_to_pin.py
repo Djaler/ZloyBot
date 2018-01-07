9944b135419a5de7ee680de2edf653ca4c78c0b8
Evgeny Kovalchuk <kovalchuk.evgeny@gmail.com>
2018-01-07 10:26:42 +0200

    Рефакторинг модуля "reply_to_pin"

--- modules/reply_to_pin.py
+++ modules/reply_to_pin.py
@@ -10,13 +10,12 @@ class ReplyToPin:
 
         self.permitted_chat_filter = PermittedChatFilter([self._chat_id])
 
-        self.last_message_id = None
-
     def add_handlers(self, add_handler):
         add_handler(CommandHandler('pinned', callback=self.reply_to_pin,
                                    filters=self.permitted_chat_filter & supergroup_filter))
 
-    def reply_to_pin(self, bot, update):
+    @staticmethod
+    def reply_to_pin(bot, update):
         message = update.message
 
         chat = bot.get_chat(message.chat_id)
@@ -24,5 +23,3 @@ class ReplyToPin:
             return
 
         chat.pinned_message.reply_text('☝️️', disable_notification=True)
-
-        self.last_message_id = message.message_id
